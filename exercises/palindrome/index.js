db66e5ad88afb40a7dc4a85b08288ed881c53062
robbridges <robbridges@users.noreply.github.com>
2021-08-14 10:13:35 -0700

    Reversing int accomplished

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -10,8 +10,16 @@
 
 function palindrome(str) {
   
-  return str === str.split('').reduce((rev, char) => char + rev, '');
+  return str.split('').every((char, i) => {
+    return char === str[str.length -i -1];
+  });
   
 }
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+  
+//   return str === str.split('').reduce((rev, char) => char + rev, '');
+  
+// }
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,42 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+	const max =  Math.pow(2, 31);
+  
+	
+	
+  let reversedNumber = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  return parseInt(reversedNumber) * Math.sign(n);
+  
+  
+  
+  
+  
+}
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+// 	const max =  Math.pow(2, 31);
+  
+	
+	
+//   let reversedNumber = parseInt(n.toString().split('').reverse().join(''),10) * Math.sign(n);
+  
+  
+//   if (reversedNumber > max || reversedNumber < -max) { 
+//   	console.log('out of bounds');
+//     return 0;
+//   }
+  
+  
+//   console.log(reversedNumber);
+//   return reversedNumber;
+  
+// }
