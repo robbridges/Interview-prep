b32d571f00576e43162fc6030ec0f11226ec979b
robbridges <robbridges@users.noreply.github.com>
2021-08-20 20:09:02 -0700

    Fibonnoci sequence done

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,15 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(num) {
+  const result = [0,1];
+
+  for (let i = 2; i <= num; i++) {
+    const a = result[i - 1];
+    const b = result[i - 2];
+    result.push(a + b);
+  }
+  return result[num];
+}
 
 module.exports = fib;
